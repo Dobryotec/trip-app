7b01983a2795127504cadfc7a5590cc91f0a3a8d
Dobrobatko Dmytro <dobrobatka@gmail.com>
2024-02-23 16:26:32 +0200

    update key

--- src/utils/fetchWeatherDayData.js
+++ src/utils/fetchWeatherDayData.js
@@ -1,7 +1,7 @@
 export const fetchWeatherDayData = async (city) => {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=days&key=ZQ3T8ZRFKDNMM3NTGSEH4XBJT&contentType=json`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=days&key=PGSEM7NAYNKWDHUMQMMHQ6KLF&contentType=json`
     );
     const data = await response.json();
     return data.days[0];
--- src/utils/fetchWetherData.js
+++ src/utils/fetchWetherData.js
@@ -1,7 +1,7 @@
 export const fetchWeatherData = async (city, startDate, endDate) => {
       try {
         const response = await fetch(
-          `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${startDate}/${endDate}?unitGroup=metric&include=days&key=ZQ3T8ZRFKDNMM3NTGSEH4XBJT&contentType=json`
+          `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${startDate}/${endDate}?unitGroup=metric&include=days&key=PGSEM7NAYNKWDHUMQMMHQ6KLF&contentType=json`
         );
         if (!response.ok) {
           throw new Error("Network response was not ok");
